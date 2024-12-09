1646bd343c125e71627e8f995fc272ed90b335ac
enjie-dev <enjie.dev@gmail.com>
2024-12-09 04:22:33 +0300

    I'V ADDED  ON CHATCONTAINER NOW WE CAN SEE IAMGES SENT AND TIME

--- front/src/components/ChatContainer.jsx
+++ front/src/components/ChatContainer.jsx
@@ -8,8 +8,10 @@ import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
 
 const ChatContainer = () => {
-    const {messages, getMessages, isMessagesLoading,selectedUser} = useChatStore()
+    const {messages, getMessages, isMessagesLoading, selectedUser} = useChatStore();
+
       const { authUser } = useAuthStore();
+      
     
 
     useEffect(() => {
@@ -23,20 +25,53 @@ const ChatContainer = () => {
             <ChatHeader />
             <MessageSkeleton />
             <MessageInput  />
+           
+            </div>
+            
+          );
 
-        </div>
-    );
+        } 
 
-}
-    
-
-
-    
-  return (
+          
+     return (
     <div className="flex-1 flex flex-col overflow-auto">
         <ChatHeader />
 
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((message) =>  (
+          
+          <div
+           key={message._id} 
+          
+          className={`chat ${message.senderId === authUser._id ? "chat-end": "chat-start"}`}>
+            <div className=" chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img 
+                
+                src={message.senderId === authUser._id ? authUser.profilePic  || "/avatar.png" 
+                  : selectedUser.profilePic || "/avatar.png"
+                }
+
+                 alt="profile pic"
+                 
+                 />
+
+
+              </div>
+            </div>
+
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {message.createdAt}
+                </time>
+
+         
+            </div>
+
+
+
+          </div>
+        ))}
 
 
          
